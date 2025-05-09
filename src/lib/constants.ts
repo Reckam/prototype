0af5544c81e76dd7f1f28c27cf6f6935162d92e5
Reckam <reckam6@gmail.com>
2025-05-09 17:25:49 +0000

    Change the name from tytc saving central to tytc saving scheme

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
-export const APP_NAME = "TYTC SAVINGS CENTRAL";
+export const APP_NAME = "TYTC SAVING SCHEME";
 export const USER_STORAGE_KEY = "savings_central_user";
 export const ADMIN_STORAGE_KEY = "savings_central_admin";
+
 
